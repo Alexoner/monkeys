fd7171090fd377e15808de06707ef01ca13126c5
H <haoisworking@gmail.com>
2024-05-25 18:55:39 +0000

    add homepage, ... comments

--- wikipedia/centered_content_width_1200px.js
+++ wikipedia/centered_content_width_1200px.js
@@ -6,6 +6,9 @@
 // @author       @Alexoner
 // @match        https://en.wikipedia.org/wiki/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
+// @homepage     https://raw.githubusercontent.com/Alexoner/monkeys/main/wikipedia
+// @downloadURL  https://raw.githubusercontent.com/Alexoner/monkeys/main/wikipedia/centered_content_width_1200px.js
+// @updateURL    https://raw.githubusercontent.com/Alexoner/monkeys/main/wikipedia/centered_content_width_1200px.js
 // @grant        none
 // ==/UserScript==
 
